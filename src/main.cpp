6d4162f691aebf244352902870bf7e0d9ddf46a5
Jason Ball <jasonericball@gmail.com>
2019-09-04 11:36:04 -0400

    Add basic web server

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,68 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <unistd.h>
+const uint16_t PORT = 8080u;
+const int SERVER_RUNNING = 1;
+const unsigned int MAX_CONNECTIONS = 10;
+const unsigned int BUFFER_SIZE = 30000;
+const char *TEST_MESSAGE = "HTTP/1.1 200 OK\r\n" \
+                           "Content-Type: text/plain\r\n" \
+                           "Content-Length: 12\r\n" \
+                           "\r\n" \
+                           "Hello World!";
+
 int main()
 {
+    int server_fd, new_socket;
+    struct sockaddr_in address{};
+    size_t addr_len;
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Error creating server socket");
+        return 1;
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
+    address.sin_addr.s_addr = INADDR_ANY;
+    memset(&address.sin_zero, 0, sizeof(address.sin_zero));
+    addr_len = sizeof(address);
+
+    if (bind(server_fd, (const sockaddr *) &address, sizeof(address)))
+    {
+        perror("Could not bind");
+        return 2;
+    }
+
+    if (listen(server_fd, MAX_CONNECTIONS) < 0)
+    {
+        perror("Could not listen");
+        return 3;
+    }
+
+    while(SERVER_RUNNING)
+    {
+        std::cout << "Waiting for a connection...\n" << std::endl;
+
+        if ((new_socket = accept(server_fd, (struct sockaddr *) &address, (socklen_t *) &addr_len)) < 0)
+        {
+            perror("Error accepting");
+            return 4;
+        }
+
+        char buffer[BUFFER_SIZE] = {0};
+        read(new_socket, buffer, BUFFER_SIZE);
+        std::cout << "-------BEGIN HTTP REQUEST-------" << std::endl << buffer << std::endl << "--------END HTTP REQUEST--------" << std::endl;
+        write(new_socket, TEST_MESSAGE, strlen(TEST_MESSAGE));
+        close(new_socket);
+    }
+
+
     return 0;
 }
